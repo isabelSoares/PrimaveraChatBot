e59c11894825552878865450061c28f8c56bfda3
Isabel Soares <isabel.srsoares@gmail.com>
2021-08-05 16:47:59 +0100

    mini improvements

--- actions/actions.py
+++ actions/actions.py
@@ -11,7 +11,7 @@ from typing import Any, Text, Dict, List
 
 from rasa_sdk import Action, Tracker
 from rasa_sdk.executor import CollectingDispatcher
-from rasa_sdk forms import FormAction
+#from rasa_sdk forms import FormAction
 
 class ActionHelloWorld(Action):
 
